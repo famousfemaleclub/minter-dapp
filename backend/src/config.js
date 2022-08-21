7aaed6d29fc5b0b7d1768a430ef9d43634735f70
famousfemaleclub <famousfemaleclubnft@gmail.com>
2022-08-21 00:59:37 -0400

    updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,10 +60,10 @@ const MINT_PRICE = 0.00; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH
 const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-20T09:45:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-08-31T09:45:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-21T09:45:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x9B3e1eb54b5f27fD0dAFddFEb923070D8Fe185DA"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
